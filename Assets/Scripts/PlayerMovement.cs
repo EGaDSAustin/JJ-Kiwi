7fc1968b8b81ff7a7bc4c30388e9e943b9e55624
Jared Kahl <kahljared4@gmail.com>
2023-12-18 13:11:00 -0600

    Made jump dynamic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,22 @@ public class Movement : MonoBehaviour
     public Transform head;
     public Rigidbody rb;
     public float lookSpeed = 5f;
-    public float moveSpeed = 2.5f;
-    public float sprintSpeed = 5f;
+    public float moveSpeed = 5f;
+    public float sprintSpeed = 10f;
 
     private float rotationX = 0f;
+
+    [SerializeField] private float JumpForce = 2f;
+    [SerializeField] private float ContinuousJumpForce = 0.8f;
+    [SerializeField] private float FallForce = 0.2f;
+    [SerializeField] private float JumpTime = 0.20f;
+
+
+    private bool jumpPressed = false;
+    private bool jumpHeld = false;
+    private bool jumpReleased = false;
+    private bool isJumping = false;
+
 
     private void Start()
     {
@@ -22,6 +34,14 @@ public class Movement : MonoBehaviour
     {
         Look();
         Move();
+    }
+
+    private void FixedUpdate()
+    {
+        if (jumpPressed) OnJumpPressed();
+        if (jumpHeld && isJumping) ContinuousJump();
+        if (rb.velocity.y < 0f) ApplyFallForce();
+        if (jumpReleased) OnJumpReleased();
     }
 
     private void Look()
@@ -47,9 +67,57 @@ public class Movement : MonoBehaviour
         Vector3 movement = (transform.right * horizontal + transform.forward * vertical).normalized;
         rb.velocity = new Vector3((movement * speed).x, rb.velocity.y, (movement * speed).z);
 
-        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(new Ray(transform.position, Vector3.down), 1.1f, 1 << 0))
-            rb.velocity += Vector3.up * 5;
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        { 
+            jumpPressed = true;
+            jumpHeld = true; 
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            jumpPressed = false;
+            jumpHeld = false;
+            jumpReleased = true;
+        }
+
 
         //rb.MovePosition(rb.position + movement * speed * Time.deltaTime); // very bad
+    }
+
+    private void OnJumpPressed()
+    {
+        jumpPressed = false;
+        if (Physics.Raycast(new Ray(transform.position, Vector3.down), 1.1f, 1 << 0)) 
+        {
+            isJumping = true;
+            rb.velocity += Vector3.up * JumpForce;
+            IEnumerator stopJumpRoutine = StopContinuousJump(JumpTime);
+            StartCoroutine(stopJumpRoutine);
+        }
+    }
+
+    private void ContinuousJump() 
+    {
+        Debug.Log("Applying continuous jump force");
+        rb.velocity += Vector3.up * ContinuousJumpForce;
+    }
+
+    private void ApplyFallForce() 
+    {
+        rb.velocity += Vector3.down * FallForce;
+    }
+
+    private void OnJumpReleased() 
+    {
+        jumpReleased = false;
+        if (rb.velocity.y > 0) rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+    }
+
+    IEnumerator StopContinuousJump(float jumpTime) 
+    {
+        Debug.Log("Stopping continuous jump force routine started");
+        yield return new WaitForSeconds(jumpTime);
+        Debug.Log("Stopping continuous jump force");
+        isJumping = false;
     }
 }
